1862bd767164cca91fdd3d1f8b8e5c282c993a28
ocfbnj <ocfbnj@qq.com>
2022-03-27 14:19:25 +0800

    Initial Commit

---
+++ Iteration/IterationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IterationApp.swift
+//  Iteration
+//
+//  Created by jiangchao on 2022/3/27.
+//
+
+import SwiftUI
+
+@main
+struct IterationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
